42f22dd1ae5de868e15a951a853a97757d11b522
JakubFraczek <kuba.fraczek@gmail.com>
2017-02-02 20:16:50 +0100

    szablon listy

--- algorithms/src/main/java/main/Main.java
+++ algorithms/src/main/java/main/Main.java
@@ -1,17 +1,11 @@
 package main;
 
-import algorithms.Palindrome;
-import algorithms.Piramid;
-
 public class Main {
 
 	public static void main(String[] args) {
 		UserInput input = new UserInput();
 
-//		Piramid piramid = new Piramid();
-//		piramid.piramidDrow(input.userIntInput());
 		
-		Palindrome palindrome = new Palindrome();
-		palindrome.isPalindrome(input.userStringInput());
+		
 	}
 }
---
+++ algorithms/src/main/java/myList/MyList.java
@@ -0,0 +1,10 @@
+package myList;
+
+public class MyList {
+	
+	Node mHead;
+	int mListSize;
+	
+
+
+}
