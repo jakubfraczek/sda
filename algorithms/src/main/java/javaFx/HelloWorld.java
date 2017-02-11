8c33fb2546686f47b5be73538b008dc180160da4
JakubFraczek <kuba.fraczek@gmail.com>
2017-02-11 11:28:26 +0100

    Pierwszy działający guzik

--- algorithms/src/main/java/javaFx/HelloWorld.java
+++ algorithms/src/main/java/javaFx/HelloWorld.java
@@ -1,10 +1,37 @@
 package javaFx;
 
-public class HelloWorld {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class HelloWorld extends Application {
 
+	public static void main(final String[] args) {
+		Application.launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button parent = new Button("Hello world!");
+		parent.setOnAction((ActionEvent) -> System.out.println("Dupa!"));
+		Scene scene = new Scene(parent);
+
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
+	}
+	
+	class ButtonAction implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		
+	}
 }
