01c37fec32a2825eea17b9b63c69a5e7774e3aae
Jakub Frączek <kuba.fraczek@gmail.com>
2017-02-25 12:35:53 +0100

    bad example of logger

---
+++ algorithms/src/main/java/aplicationServers/JULDemo.java
@@ -0,0 +1,14 @@
+package aplicationServers;
+
+import java.util.logging.Logger;
+
+public class JULDemo {
+
+	static final Logger logger = Logger.getLogger("JULDemo");
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
